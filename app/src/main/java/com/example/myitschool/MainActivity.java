c3025b088bf49df8acbc46169eb74ac1a179a5ad
Kirill Domashenko <SunZar_Kirill@mail.ru>
2023-01-27 23:26:15 +0700

    Save and load progress

--- app/src/main/java/com/example/myitschool/MainActivity.java
+++ app/src/main/java/com/example/myitschool/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.myitschool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.myitschool.databinding.ActivityMainBinding;
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private String moneyStr;
     private int money;
+
+    SharedPreferences mySP;
+    final int SAVE_INT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
                 binding.moneyCount.setText(moneyStr);
             }
         });
+        binding.buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savePref();
+            }
+        });
+        binding.buttonLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadPref();
+            }
+        });
+    }
+
+    private void savePref() {
+        mySP = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = mySP.edit();
+        editor.putInt(String.valueOf(SAVE_INT), money);
+        editor.commit();
+        Toast.makeText(this, "Progress saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadPref() {
+        mySP = getPreferences(MODE_PRIVATE);
+        int saveInt = mySP.getInt(String.valueOf(SAVE_INT), 0);
+        binding.moneyCount.setText(Integer.toString(saveInt));
+        Toast.makeText(this, "Progress loaded", Toast.LENGTH_SHORT).show();
     }
 }
